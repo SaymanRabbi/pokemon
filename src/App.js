52d75922869334c7c2b8ad98a2153c4e4323a8bb
sayman <mdrabbi.asm@gmail.com>
2022-06-21 23:00:18 +0600

    cancel method add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Pokemonlist from "./Components/Pokemonlist/Pokemonlist";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loading from "./Components/Pokemonlist/Loading";
+import axios from "axios";
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl,setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
@@ -10,15 +11,19 @@ function App() {
   const [loading,setLoading]=useState(true)
   useEffect(() => {
     setLoading(true)
-    fetch(currentPageUrl).then(res => res.json()).then(data => {
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken:new axios.CancelToken(c=>cancel=c)
+    }).then(res => {
       setLoading(false)
-      setPokemon(data?.results)
-      setNextPageUrl(data?.next)
-      setPrevPageUrl(data?.previous)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results)
     })
+    return()=>cancel.cancel()
   },[currentPageUrl])
   if (loading) {
-  return <Loading/>
+    return <Loading />
 }
   return (
     < >
